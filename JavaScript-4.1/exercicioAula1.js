932ce77c7fede444d8e13f4d86cb9b607886b4fd
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-07-20 15:55:53 -0300

    adiciona exercicio da aula1 bloco3

---
+++ JavaScript-4.1/exercicioAula1.js
@@ -0,0 +1,11 @@
+const myName = "Thais";
+const birthCity = "São José dos Campos";
+let birthYear = "1990";
+birthYear = "2030";
+birthCity = "Jacarei"
+console.log( myName) 
+console.log(birthCity) 
+console.log(birthYear)
+
+
+
