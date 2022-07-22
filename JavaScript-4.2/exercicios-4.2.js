f2f5d65df39c5bbc2662af723a8d26100f351087
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-07-22 15:52:48 -0300

    adiciona os exercicios 4.2

--- JavaScript-4.2/exercicios-4.2.js
+++ JavaScript-4.2/exercicios-4.2.js
@@ -79,3 +79,10 @@
 //     novoArray.push(number/2)
 // }
 // console.log(novoArray)
+
+
+let resultado = 0;
+for (let index = 0; index <= 100; index += 1) {
+  resultado += index;
+};
+console.log(resultado);
