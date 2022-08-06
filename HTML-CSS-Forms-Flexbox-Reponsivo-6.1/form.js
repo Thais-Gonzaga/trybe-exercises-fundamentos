bcec46aba24fca1dfd0e683d9ef6d62f2f3af046
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-05 23:58:33 -0300

    adiciona exercicio uma parte do exercicio bonus do 6.1

--- HTML-CSS-Forms-Flexbox-Reponsivo-6.1/form.js
+++ HTML-CSS-Forms-Flexbox-Reponsivo-6.1/form.js
@@ -2,5 +2,12 @@
 const buttonEnviar = document.querySelector('#enviar');
 
 buttonEnviar.addEventListener('click', function enviar(event) {
-    event.preventDefault();
+  event.preventDefault();
 });
+//bonus
+//Faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+function check(){
+  const concordo = document.querySelector('#concordo');
+  buttonEnviar.disabled = !concordo.check;
+ }
+
